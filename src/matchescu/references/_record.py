638474d1fdd0c5e15b5f667d6100bab25d808b99
Andrei Olar <andrei.olar@gmail.com>
2025-03-24 07:16:11 +0200

    Implement as_dict on the standard EntityReference data type

--- src/matchescu/references/_record.py
+++ src/matchescu/references/_record.py
@@ -10,3 +10,9 @@ class EntityReference(Record):
     def __init__(self, identifier: EntityReferenceIdentifier, value: Iterable):
         super().__init__(value)
         self.id = identifier
+
+    def as_dict(self) -> dict:
+        return {
+            attr_name: self._attr_values[attr_idx]
+            for attr_name, attr_idx in self._attr_names.items()
+        }
